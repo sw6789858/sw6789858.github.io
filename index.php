7eeb5b2d019bfdf0477f7198c2abdc1288093bf5
sw6789858 <141367134+sw6789858@users.noreply.github.com>
2023-08-08 09:46:35 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,223 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <!-- Site made with Mobirise Website Builder v5.3.10, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v5.3.10, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/2763-128x85.png" type="image/x-icon">
+  <meta name="description" content="European quarter in Nha Trang">
+  
+  
+  <title>European quarter in Nha Trang</title>
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/web/assets/gdpr-plugin/gdpr-styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'">
+  <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Jost:100,200,300,400,500,600,700,800,900,100i,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"></noscript>
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+  
+</head>
+<body>
+  
+  <section class="header1 cid-sKMUGPYr0K mbr-fullscreen mbr-parallax-background" id="header1-fv">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.9; background-color: rgb(0, 0, 0);"></div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-9">
+                <h1 class="mbr-section-title mbr-fonts-style mb-3 display-1"><strong>European quarter </strong><br><strong>in Nha Trang</strong></h1>
+                
+                
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content4 cid-tKgD8Pq7F7" id="content4-yz">
+    
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="title col-md-12 col-lg-10">
+                <h3 class="mbr-section-title mbr-fonts-style align-center mb-4 display-2"><strong>Vietnam is often referred</strong></h3>
+                <h4 class="mbr-section-subtitle align-center mbr-fonts-style mb-4 display-7">To as the land of dragons and fairies. The name of the state can be translated as "the country of the southern Vietnamese", but until 1945 it was called Annam, and its current name was used in the literary language. It was made official by Emperor Bao Dai. Today we offer you a virtual tour of the most developed and popular resort in the country, a place that has many opportunities for great recreation and a variety of entertainment - the city of Nha Trang, located in the south of Vietnam.</h4>
+                
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content6 cid-tKgD8ZUeRZ" id="content6-z0">
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-10">
+                <hr class="line">
+                <p class="mbr-text align-center mbr-fonts-style my-4 display-7">We invite everyone who has been to this resort to join our conversation and, in the comments to the article, tell us about your impressions of the city, which has changed in recent years: many hotels, bars and cafes have appeared there, and the popularity of the resort is growing every year</p>
+                <hr class="line">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="image4 cid-tK5ec2UKh6" id="image4-yb">
+    
+
+    
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-lg-6">
+                <div class="image-wrapper">
+                    <img src="assets/images/b2f02a79fde779258a2f68e630457fe6-1024x683.jpg" alt="Mobirise">
+                    
+                </div>
+            </div>
+            <div class="col-12 col-lg-6">
+                <div class="image-wrapper">
+                    <img src="assets/images/115955-2-1024x683.jpg" alt="Mobirise">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content15 cid-tKgD9qkOWh" id="content15-z1">
+
+    
+
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="card col-md-12 col-lg-10">
+                <div class="card-wrapper">
+                    <div class="card-box align-left">
+                        <h4 class="card-title mbr-fonts-style mbr-white mb-3 display-5"><strong>Districts of Nha Trang</strong></h4>
+                        <p class="mbr-text mbr-fonts-style display-7">City is conditionally divided into three districts: South. Central. Northern. Next, we will briefly describe each of them. City South It is located a 30-minute drive from Cam Ranh Airport and the pier, from which boat trips depart daily. There is also a cable car to the VinPearl entertainment island. The area is calm and quiet, despite the fact that there are noisy discos in Central Park. There are many small budget hotels and cafes here. One of the most popular is a cafe in Central Park - "Louisiana". Of the attractions located in this part of the city, many tourists recommend visiting: Central Park. Villas of Bao Dai. Museum of Oceanography. If some interesting place impressed you more, advise it to travelers who are going to Nha Trang for the first time.</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="image3 cid-tKgD9NHPqv" id="image3-z2">
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10">
+                <div class="image-wrapper">
+                    <img src="assets/images/fe2026a2b7f48e31915a350bf1a7388e-1200x799.jpg" alt="Mobirise">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content7 cid-tKgDbW4N3T" id="content7-z3">
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md-10">
+                <blockquote>
+                <h5 class="mbr-section-title mbr-fonts-style mb-2 display-5"><strong>Central area Judging by the reviews</strong></h5>
+                <p class="mbr-text mbr-fonts-style display-4">Busiest place in Nha Trang. The European quarter, which among our compatriots is known as Russian, in which the number of bars, cafes, shops and restaurants per square meter just rolls over, is located here. The Central District is crowded and noisy, there are many tourists from Russia, there is always somewhere to go - everything is within walking distance: minimarkets and fruit shops, night markets. There are many hotels of different levels of comfort in this area, including chain hotels - Best Western, Sheraton, Novotel and others.</p></blockquote>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content10 cid-tKgDccxYUn" id="content10-z4">
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-md">
+                
+                <h6 class="mbr-section-subtitle mbr-fonts-style mb-4 display-5">Beach is more crowded</h6>
+            </div>
+            <div class="col-md-12 col-lg-6">
+                <p class="mbr-text mbr-fonts-style display-7">Northern area It is located behind the bridge, an hour's drive from the airport. The area is calm, somewhat remote from the center. If you plan to take a taxi every day, then living here is expensive. The sea in the bay is very calm, and the beach delights with cleanliness. There are not too many hotels here, usually houses are rented in this area. European Quarter in Nha Trang First, let's figure out where it is located. The fact is that in this city there is no line of demarcation of districts. <br><br>However, conditionally, the first three lines from the famous Lotus, located on the embankment, are referred to this site. The European quarter in Nha Trang got its name due to the fact that tourists with a European appearance like to relax here: Americans, Slavs, Europeans. The first, second and third lines of the European quarter serve as a reference point. <br><br>On the opposite side of the European quarter of Nha Trang there is a wonderful green square with gazebos, exercise equipment and benches. One of the most popular restaurants in the city, Sailing Club Bar, operates on the very seashore. The local beach is wide and clean, but the closer to the Lotus tower, the more noisy and crowded it becomes. What to see in Nha Trang on your own? This city has many architectural and historical sights. Many of them can be explored on your own, as they are located within walking distance. We will present you some of them.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="image3 cid-tKaSb4Cuq8" id="image3-yt">
+    
+
+    
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-10">
+                <div class="image-wrapper">
+                    <img src="assets/images/vietnam-nja-chang-24535-1200x800.jpg" alt="Mobirise">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content5 cid-tKaT1e3B1Q" id="content5-yx">
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-10">
+                <h3 class="mbr-section-title mbr-fonts-style mb-4 display-2"><strong>Cathedral Built at the beginning</strong></h3>
+                
+                <p class="mbr-text mbr-fonts-style display-7">Of the last century, the church is in the Gothic style, familiar to Europeans. A tall belfry adorns the building. It has a number of traditional small windows and a dial. Crowning the structure is a stone cross. The height of the cathedral, where nowadays the residence of the ruling bishop is located, is almost 40 meters. Its windows are decorated with magnificent stained glass windows made of colored transparent glass. They fill the building with bright sunlight. Three large bells, which were cast by local craftsmen from copper, were installed on the tower two years after the construction was completed. Work on the chimes continued until 1935. Today it is one of the magnificent Catholic complexes located in Southeast Asia.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="content5 cid-tKgE0AFnJj" id="content5-z5">
+    
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12 col-lg-10">
+                
+                <h4 class="mbr-section-subtitle mbr-fonts-style mb-4 display-5">Central Park</h4>
+                <p class="mbr-text mbr-fonts-style display-7">Due to its focus on Russian-speaking tourists, the park has acquired the semi-official name "Gorky Park". It is located within walking distance from the Cathedral. Not only guests of the city, but also locals like to rest here. The park works without weekends and practically around the clock - it closes only before dawn for a few hours. During this time, employees tidy up the alleys and lawns.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="footer7 cid-sFTSSt2GxL" once="footers" id="footer7-p">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row align-center mbr-white">
+            <div class="col-12">
+                <p class="mbr-text mb-0 mbr-fonts-style display-7"><a href="index.html" class="text-primary">Home</a>&nbsp;&nbsp;<a href="terms.html" class="text-primary">Terms And Conditions</a>&nbsp;&nbsp;<a href="privacy.html" class="text-primary">Privacy Policy</a>&nbsp;&nbsp;<a href="cookies.html" class="text-primary">Cookies</a></p>
+            </div>
+        </div>
+    </div>
+  
+  
+ <div id="scrollToTop" class="scrollToTop mbr-arrow-up"><a style="text-align: center;"><i class="mbr-arrow-up-icon mbr-arrow-up-icon-cm cm-icon cm-icon-smallarrow-up"></i></a></div>
+  </body>
+</html>
